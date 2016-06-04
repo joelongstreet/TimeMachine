082d86067ae49909cf84de9577691e999d7f7fbb
Joe Longstreet <joelongstreet@gmail.com>
2016-06-03 21:25:59 -0500

    Initial Commit

---
+++ TimeMachine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeMachine
+//
+//  Created by Joe Longstreet on 6/3/16.
+//  Copyright © 2016 Joe Longstreet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
