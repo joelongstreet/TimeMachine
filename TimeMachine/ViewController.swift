5782e24d2ec9e01b4b4106896b2005c22cc24d97
Joe Longstreet <joelongstreet@gmail.com>
2016-06-09 21:16:46 -0500

    rid myself of semicolons

--- TimeMachine/ViewController.swift
+++ TimeMachine/ViewController.swift
@@ -17,55 +17,55 @@ class ViewController: UIViewController, DFBlunoDelegate {
     
     
     override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated);
+        super.viewDidAppear(animated)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.blackColor();
+        self.view.backgroundColor = UIColor.blackColor()
         
         // setup the bluetooth scanner
-        self.blunoManager.delegate = self;
-        self.blunoManager.scan();
+        self.blunoManager.delegate = self
+        self.blunoManager.scan()
         
         // config
-        let width = 150.0;
-        let height = 84.0;
+        let width = 150.0
+        let height = 84.0
         
         // setup the video player and it's resource path
-        let path = NSBundle.mainBundle().pathForResource("sample", ofType: "mp4");
-        let player = AVPlayer(URL: NSURL(fileURLWithPath: path!));
+        let path = NSBundle.mainBundle().pathForResource("sample", ofType: "mp4")
+        let player = AVPlayer(URL: NSURL(fileURLWithPath: path!))
         
         // create the replicator layer
-        let replicatorLayer = CAReplicatorLayer();
-        replicatorLayer.anchorPoint = CGPoint(x: 0, y: 0);
-        replicatorLayer.bounds = CGRect(x: 0, y: 0, width: width, height: height);
-        replicatorLayer.position = CGPoint(x: height, y: 80);
-        replicatorLayer.instanceCount = 4;
+        let replicatorLayer = CAReplicatorLayer()
+        replicatorLayer.anchorPoint = CGPoint(x: 0, y: 0)
+        replicatorLayer.bounds = CGRect(x: 0, y: 0, width: width, height: height)
+        replicatorLayer.position = CGPoint(x: height, y: 80)
+        replicatorLayer.instanceCount = 4
         
         // create a transformation object
-        var transform = CATransform3DIdentity;
-        let angle:CGFloat = CGFloat(90.0 * M_PI / 180.0);
-        transform = CATransform3DRotate(transform, angle, 0, 0, 1);
-        transform = CATransform3DTranslate(transform, CGFloat(height), CGFloat(-1*(width+height)), 0);
+        var transform = CATransform3DIdentity
+        let angle:CGFloat = CGFloat(90.0 * M_PI / 180.0)
+        transform = CATransform3DRotate(transform, angle, 0, 0, 1)
+        transform = CATransform3DTranslate(transform, CGFloat(height), CGFloat(-1*(width+height)), 0)
 
         // apply the transformation to the replicator instances
-        replicatorLayer.instanceTransform = transform;
+        replicatorLayer.instanceTransform = transform
 
         // create a video player layer
-        var playerLayer = AVPlayerLayer();
-        playerLayer = AVPlayerLayer(player: player);
-        playerLayer.bounds = CGRect(x: 0, y: 0, width: width, height: height);
-        playerLayer.position = CGPoint(x: 0, y: 0.0);
-        playerLayer.anchorPoint = CGPoint(x: 0, y: 0);
+        var playerLayer = AVPlayerLayer()
+        playerLayer = AVPlayerLayer(player: player)
+        playerLayer.bounds = CGRect(x: 0, y: 0, width: width, height: height)
+        playerLayer.position = CGPoint(x: 0, y: 0.0)
+        playerLayer.anchorPoint = CGPoint(x: 0, y: 0)
         
         // add the layers to the view
-        replicatorLayer.addSublayer(playerLayer);
-        self.view.layer.addSublayer(replicatorLayer);
+        replicatorLayer.addSublayer(playerLayer)
+        self.view.layer.addSublayer(replicatorLayer)
         
         // autoplay
-        player.play();
+        player.play()
     }
 
     override func didReceiveMemoryWarning() {
@@ -74,7 +74,7 @@ class ViewController: UIViewController, DFBlunoDelegate {
     }
     
     override func prefersStatusBarHidden() -> Bool {   
-        return true;
+        return true
     }
     
     
